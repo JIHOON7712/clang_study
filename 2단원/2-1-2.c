b5196a8fa2dea8ec8503e2308e619ae981b59a82
jihoon <jihoon@jihoonui-MacBookPro.local>
2022-12-25 00:09:15 +0900

    chapter2

---
+++ 2단원/2-1-2.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+int main(void)
+{
+	printf("이름 : 황지훈\n");
+	printf("주소 : 안산시 ㅁㄴㅇㄹ\n");
+	return 0;
+}
---
+++ 2단원/2-1.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main(void)
+{
+	printf("È²ÁöÈÆ\n");
+	printf("È² Áö ÈÆ\n");
+	printf("È²  Áö  ÈÆ\n");
+	return 0;
+}
